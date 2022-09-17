fa9a43fdedf771d46c9a0e678f175016e55eb909
doozles411 <cindy.phelps01@gmail.com>
2022-09-17 15:03:37 -0700

    Render headers and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from "react";
 const Home = () => {
     return (
         <div className="home">
-            
+            <h1 className="home-header">ChillOut Co.</h1>
+            <h2 className="home-subtitle">Trade stress for self-care.</h2>
+            <h2 className="home-subtitle">Find your good vibes here.</h2>
+            <button className="home-btn">Visit the Shop</button>
         </div>
     )
 }
