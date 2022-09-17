581c485af42424aaebab99816d311a8b38798c25
doozles411 <cindy.phelps01@gmail.com>
2022-09-17 15:37:39 -0700

    Add image to header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import flowerIcon from '../assets/images/flowerIcon.png';
 
 const Home = () => {
     return (
         <div className="home">
-            <h1 className="home-header">ChillOut Co.</h1>
-            <h2 className="home-subheader">Trade stress for self-care.</h2>
-            <h2 className="home-subheader">Find your good vibes here.</h2>
+            <h1 className="home-header">ChillOut Co
+                <img className="home-img" alt="Flower icon" src={flowerIcon} />
+            </h1>
+            <h2 className="home-subheader">Trade stress for self-care</h2>
+            <h2 className="home-subheader">Find your good vibes here</h2>
             <button className="home-btn">Visit the Shop</button>
         </div>
     )
