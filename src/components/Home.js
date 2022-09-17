1b170422ba36b5148f6b0e41c108fe731e4c2839
doozles411 <cindy.phelps01@gmail.com>
2022-09-17 15:28:39 -0700

    Upload image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
     return (
         <div className="home">
             <h1 className="home-header">ChillOut Co.</h1>
-            <h2 className="home-subtitle">Trade stress for self-care.</h2>
-            <h2 className="home-subtitle">Find your good vibes here.</h2>
+            <h2 className="home-subheader">Trade stress for self-care.</h2>
+            <h2 className="home-subheader">Find your good vibes here.</h2>
             <button className="home-btn">Visit the Shop</button>
         </div>
     )
