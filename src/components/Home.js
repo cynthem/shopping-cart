babaff69dba8678f7a1e0bc5c2e161865bacb8ea
doozles411 <cindy.phelps01@gmail.com>
2022-09-18 15:47:14 -0700

    Fix attribute typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="home">
             <h1 className="home-header">ChillOut Co
-                <a href="https://github.com/doozles411" target="_blank" ref="noopener noreferrer">
+                <a href="https://github.com/doozles411" target="_blank" rel="noopener noreferrer">
                     <img className="home-img" alt="Flower icon" src={flowerIcon} />
                 </a>
             </h1>
