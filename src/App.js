19ed53103e8141edb950b99c2471526b76249c5a
doozles411 <cindy.phelps01@gmail.com>
2022-09-18 16:38:56 -0700

    Import and render Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/Header';
 import Shop from './components/Shop';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/shop" element={<Shop />} />
