1ac6baeead44de79fbd7ee529e6393770f239f09
doozles411 <cindy.phelps01@gmail.com>
2022-09-18 15:39:58 -0700

    Remove second subheader

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,7 @@ const Home = () => {
             <h1 className="home-header">ChillOut Co
                 <img className="home-img" alt="Flower icon" src={flowerIcon} />
             </h1>
-            <h2 className="home-subheader">Trade stress for self-care</h2>
-            <h2 className="home-subheader">Find your good vibes here</h2>
+            <h2 className="home-subheader">Trade stress for good vibes</h2>
             <button className="home-btn">Visit the Shop</button>
         </div>
     )
