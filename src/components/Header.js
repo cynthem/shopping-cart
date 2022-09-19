8ea7deee5c667717cfb1d02c4de9a9094b9ef872
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 13:56:01 -0700

    Change Header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,17 @@ import basketIcon from '../assets/images/basketIcon.png';
 const Header = () => {
     return (
         <div className="header">
-            <div className="header-navbar">
-                <Link className="header-home" to="/">Home</Link>
-                <Link className="header-shop" to="/shop">Shop</Link>
-                <Link className="basket-icon" to="/cart">
-                    <img className="basket-icon" alt="Basket" src={basketIcon} />
-                </Link>
-            </div>
-            <div className="header-title">
-                <p>ChillOut Co.</p>
+            <div className="header-container">
+                <div className="header-navbar">
+                    <Link className="header-home" to="/">Home</Link>
+                    <Link className="header-shop" to="/shop">Shop</Link>
+                    <Link className="basket-icon" to="/cart">
+                        <img className="basket-icon" alt="Basket" src={basketIcon} />
+                    </Link>
+                </div>
+                <div className="header-title">
+                    <p>ChillOut Co.</p>
+                </div>
             </div>
         </div>
     )
