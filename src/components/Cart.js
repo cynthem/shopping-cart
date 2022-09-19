d12221205384b2ec0fa83443f3f7ae24de4ce626
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 11:41:51 -0700

    Export Cart

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            Basket
+        </div>
+    )
+}
+
+export default Cart;
