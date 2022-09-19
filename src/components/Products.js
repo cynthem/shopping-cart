6091e603abad761d6344e63cf206596daf882e6a
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 13:09:27 -0700

    Upload image

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Products = () => {
+    const products = [
+        {
+            name: "Kitten time",
+            price: 29.50
+        }
+    ];
+
     return (
         <div className="products">
             product
