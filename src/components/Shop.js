d13362e3d1c7a8cc0c3bdafe1720f8abf87b708e
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 12:00:45 -0700

    Import Products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Products from './components/Products';
 
 const Shop = () => {
     return (
         <div className="shop">
-            
+            <Products />
         </div>
     )
 }
