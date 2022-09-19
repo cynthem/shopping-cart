d3704098056f357e51ab9b291528860e2402988b
doozles411 <cindy.phelps01@gmail.com>
2022-09-18 17:14:21 -0700

    Add header hover effects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import basketIcon from '../assets/images/basketIcon.png';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="header-navbar">
-                <p>Home</p>
-                <p>Shop</p>
-                <img className="basket-icon" alt="Basket" src={basketIcon} />
+                <Link className="header-home" to="/">Home</Link>
+                <Link className="header-shop" to="/shop">Shop</Link>
+                <Link className="basket-icon" to="/cart">
+                    <img className="basket-icon" alt="Basket" src={basketIcon} />
+                </Link>
             </div>
             <div className="header-title">
                 <p>ChillOut Co.</p>
