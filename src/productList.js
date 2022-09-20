8e4495c9a3f6e7ae5c98ee1729e809e0347b3e45
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 18:13:04 -0700

    Complete product list

--- src/productList.js
+++ src/productList.js
@@ -1,6 +1,15 @@
 import kittens from './assets/images/products/kittens.jpg';
 import bath from './assets/images/products/bath.jpg';
 import hugs from './assets/images/products/hugs.jpg';
+import therapy from './assets/images/products/therapy.jpg';
+import donuts from './assets/images/products/donuts.jpg';
+import scalp from './assets/images/products/scalp.jpg';
+import assistant from './assets/images/products/assistant.jpg';
+import headphones from './assets/images/products/headphones.jpg';
+import voodoo from './assets/images/products/voodoo.jpg';
+import silk from './assets/images/products/silk.jpg';
+import massage from './assets/images/products/massage.jpg';
+import coloring from './assets/images/products/coloring.jpg';
 
 const productList = [
     {
@@ -23,6 +32,69 @@ const productList = [
         rate: "gas money",
         image: hugs,
         description: "Hard day at work? Grandma will make it better with one of her famous hugs! Depending on how badly the day went, she might lend an ear to your problems and console you (plus more hugs!). This service is free but Grandma asks that you provide gas money and maybe some milk for her cookies."
+    },
+    {
+        name: "Therapy",
+        price: 125.00,
+        rate: "per session",
+        image: therapy,
+        description: "Care to share? Spill your guts to a certified therapist weekly or bi-weekly and tackle your stress with a long-term approach. These private sessions will provide the space to trade your ineffective coping mechanisms with productive ways of handling life's stressors."
+    },
+    {
+        name: "Donut Subscription",
+        price: 19.99,
+        rate: "per week",
+        image: donuts,
+        description: "The only worry you'll feel Monday mornings is whether your weekly donut subscription will last through Friday. And hey, it's okay if it doesn't! You'll have half-a-dozen delicious donuts to snack on that will ease your worries in no time. Flavors are assorted and change each week."
+    },
+    {
+        name: "Scalp Massager",
+        price: 12.00,
+        rate: "",
+        image: scalp,
+        description: "You'll wonder why people don't carry scalp massagers everywhere with them once you try one. When the tingles reach down your spine, you'll understand the term 'headgasm.' This item is one of the best investments you'll ever make!"
+    },
+    {
+        name: "Personal Assistant",
+        price: 30.00,
+        rate: "per hour",
+        image: assistant,
+        description: "While not cheap, we can all still agree a personal assistant is so worth it. Our trained and well-qualified personal assistants will take care of those time-consuming tasks, chores, and errands that seem to eat up your free time. They'll even do your taxes!"
+    },
+    {
+        name: "Noise Cancelling Headphones",
+        price: 69.95,
+        rate: "",
+        image: headphones,
+        description: "When you need to turn the noise of the world off, nothing is better than noise cancelling headphones. Shut out the demands and the whining and listen to whatever YOU want! Not recommended while driving."
+    },
+    {
+        name: "Voodoo Doll",
+        price: 7.99,
+        rate: "",
+        image: voodoo,
+        description: "Sometimes it helps to exorcise the stress demons. Summon all of your worries into this doll and give it the punishment it deserves with a free pack of needles. Our spells have ensured these Voodoo dolls don't work on people or animals."
+    },
+    {
+        name: "The Silk Package",
+        price: 145.00,
+        rate: "",
+        image: silk,
+        description: "Ease into luxury every night with the silk package. Includes bedding and pajamas and is guaranteed to make you forget the worries of the day and drift off peacefully in no time. Dry clean only."
+    },
+    {
+        name: "Massage",
+        price: 85.00,
+        rate: "per hour",
+        image: massage,
+        description: "Sometimes we carry stress in our backs... and sometimes in our feet, legs, heads, and you get the picture. Enjoy a full body massage for up to an hour or more. Massage therapists will come to you. It's okay to fall asleep."
+    },
+    {
+        name: "Coloring Book",
+        price: 9.99,
+        rate: "",
+        image: coloring,
+        description: "One of the simplest - but effective - ways to unwind after a long day (or week) is with a classic coloring book. We recommend coloring pencils, but pages can be easily removed and colored with paint or markers. Coloring supplies not included."
     }
 ];
 
