ed2b0158a35d6a0c1f112961a06fbe812ebeaa9f
doozles411 <cindy.phelps01@gmail.com>
2022-09-19 18:27:51 -0700

    Edit product descriptions

--- src/productList.js
+++ src/productList.js
@@ -31,21 +31,21 @@ const productList = [
         price: "??",
         rate: "gas money",
         image: hugs,
-        description: "Hard day at work? Grandma will make it better with one of her famous hugs! Depending on how badly the day went, she might lend an ear to your problems and console you (plus more hugs!). This service is free but Grandma asks that you provide gas money and maybe some milk for her cookies."
+        description: "Hard day at work? Grandma will make it better with one of her famous hugs! Depending on how rough the day was, she might lend an ear to your problems and console you (plus more hugs!). This service is free but Grandma asks that you provide gas money and maybe some milk for the cookies she plans to bring."
     },
     {
         name: "Therapy",
         price: 125.00,
         rate: "per session",
         image: therapy,
-        description: "Care to share? Spill your guts to a certified therapist weekly or bi-weekly and tackle your stress with a long-term approach. These private sessions will provide the space to trade your ineffective coping mechanisms with productive ways of handling life's stressors."
+        description: "Care to share? Spill your guts to a certified therapist weekly or bi-weekly and tackle your stress with a long-term approach. These private sessions will provide the space to trade your ineffective coping mechanisms with more productive ways to handle life's stressors."
     },
     {
         name: "Donut Subscription",
         price: 19.99,
         rate: "per week",
         image: donuts,
-        description: "The only worry you'll feel Monday mornings is whether your weekly donut subscription will last through Friday. And hey, it's okay if it doesn't! You'll have half-a-dozen delicious donuts to snack on that will ease your worries in no time. Flavors are assorted and change each week."
+        description: "The only worry you'll feel Monday mornings is whether your weekly donut subscription will last through Friday. And hey, it's okay if it doesn't! You'll have half of a dozen delicious donuts to ease your worries in no time. Flavors are assorted and change each week."
     },
     {
         name: "Scalp Massager",
@@ -66,35 +66,35 @@ const productList = [
         price: 69.95,
         rate: "",
         image: headphones,
-        description: "When you need to turn the noise of the world off, nothing is better than noise cancelling headphones. Shut out the demands and the whining and listen to whatever YOU want! Not recommended while driving."
+        description: "When you need to turn the noise of the world off, nothing is better than noise cancelling headphones. Shut out the demands and the whining and listen to whatever YOU want! Best if used in the office... and in any kind of office. Not recommended while driving."
     },
     {
         name: "Voodoo Doll",
         price: 7.99,
         rate: "",
         image: voodoo,
-        description: "Sometimes it helps to exorcise the stress demons. Summon all of your worries into this doll and give it the punishment it deserves with a free pack of needles. Our spells have ensured these Voodoo dolls don't work on people or animals."
+        description: "Sometimes it helps to exorcise the stress demons. Summon all of your worries into this doll and give it the punishment it deserves with a complimentary pack of needles. Our spells have ensured these Voodoo dolls don't work on people or animals."
     },
     {
         name: "The Silk Package",
         price: 145.00,
         rate: "",
         image: silk,
-        description: "Ease into luxury every night with the silk package. Includes bedding and pajamas and is guaranteed to make you forget the worries of the day and drift off peacefully in no time. Dry clean only."
+        description: "Ease into luxury every night with the silk package. Includes silk bedding and pajamas and is guaranteed to make you forget the worries of the day and drift off to a peaceful sleep in no time. Available in several colors. Dry clean only."
     },
     {
         name: "Massage",
         price: 85.00,
         rate: "per hour",
         image: massage,
-        description: "Sometimes we carry stress in our backs... and sometimes in our feet, legs, heads, and you get the picture. Enjoy a full body massage for up to an hour or more. Massage therapists will come to you. It's okay to fall asleep."
+        description: "Sometimes we carry stress in our backs... and sometimes in our feet, legs, heads, and... you get the picture. Enjoy a full body massage for up to an hour or as long as you need! Massage therapists will come to you. And it's okay to fall asleep."
     },
     {
         name: "Coloring Book",
         price: 9.99,
         rate: "",
         image: coloring,
-        description: "One of the simplest - but effective - ways to unwind after a long day (or week) is with a classic coloring book. We recommend coloring pencils, but pages can be easily removed and colored with paint or markers. Coloring supplies not included."
+        description: "One of the simplest - but effective - ways to unwind after a long day (or week) is with a classic coloring book. We recommend filling in with coloring pencils, but pages can be easily removed and colored with paint or markers. Coloring supplies not included."
     }
 ];
 
