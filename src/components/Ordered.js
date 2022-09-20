2e419cace97a45a52f2f4c9d33453d5e16dbc7b2
doozles411 <cindy.phelps01@gmail.com>
2022-09-20 14:14:56 -0700

    Wrap icon with Link

--- src/components/Ordered.js
+++ src/components/Ordered.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import flowerIcon from '../assets/images/flowerIcon.png';
 
 const Ordered = () => {
@@ -7,7 +8,9 @@ const Ordered = () => {
             <h2 className="order-title">Thanks for ordering!</h2>
             <p className="order-text-1">Unfortunately, we will not be able to ship your items as we lack shipping capabilities and there are no items to ship and this is all fake anyway.</p>
             <p className="order-text-2">The items you have ordered are not impossible to obtain elsewhere. Try giving Grandma a call, check Amazon, or ask for therapy/massage recommendations, etc.</p>
-            <img className="order-img" src={flowerIcon} alt="Flower icon" />
+            <Link className="order-img-link" to="/">
+                <img className="order-img" src={flowerIcon} alt="Flower icon" />     
+            </Link>
             <p className="order-text-3">May you live peacefully with a healthy amount of stress.</p>
         </div>
     )
