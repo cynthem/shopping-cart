3005167a7df1ee751b101bb0d3f41c4229d2b48a
doozles411 <cindy.phelps01@gmail.com>
2022-09-20 12:47:15 -0700

    Render Shop products within App

--- src/App.js
+++ src/App.js
@@ -4,77 +4,89 @@ import uniqid from 'uniqid';
 import Header from './components/Header';
 import Home from './components/Home';
 import Shop from './components/Shop';
-import Product from './components/Product';
 import Cart from './components/Cart';
 import Ordered from './components/Ordered';
 import productList from "./productList";
 
 function App() {
-    const [cart, setCart] = useState([]);
-    const [priceTotal, setPriceTotal] = useState(0);
-    const [productNum, setProductNum] = useState(0);
+  const [cart, setCart] = useState([]);
+  const [priceTotal, setPriceTotal] = useState(0);
+  const [productNum, setProductNum] = useState(0);
 
-    useEffect(() => {
-      let price = 0;
-      cart.forEach(e => {
-        price += e.quantity * e.product.price
+  useEffect(() => {
+    let price = 0;
+    cart.forEach(e => {
+      price += e.quantity * e.product.price
+    });
+    setPriceTotal(price);
+  }, [productNum]);
+
+  const handleAddToCart = (e) => {
+    let inCart = false;
+    if (cart.length > 0) {
+      cart.forEach((item) => {
+        if (item.product.name === e.name) {
+          item.quantity += 1;
+          setProductNum(productNum + 1);
+          inCart = true;
+        }
       });
-      setPriceTotal(price);
-    }, [productNum]);
-
-    const handleAddToCart = (e) => {
-      let inCart = false;
-      if (cart.length > 0) {
-        cart.forEach((item) => {
-          if (item.product.name === e.name) {
-            item.quantity += 1;
-            setProductNum(productNum + 1);
-            inCart = true;
-          }
-        });
-      }
-      if (!inCart) {
-        setCart([...cart, { product: e, quantity: 1 }]);
-        setProductNum(productNum + 1);
-      }
     }
+    if (!inCart) {
+      setCart([...cart, { product: e, quantity: 1 }]);
+      setProductNum(productNum + 1);
+    }
+  }
 
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header productNum={productNum} />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route 
-                        path="/shop" 
-                        element={
-                            <Shop>
-                                {productList.map(e => {
-                                    return (
-                                      <Product key={uniqid()} e={e} handleAddToCart={handleAddToCart} />
-                                    )
-                                })}
-                            </Shop>
-                        } 
-                    />
-                    <Route 
-                      path="/cart" 
-                      element={
-                          <Cart 
-                              cart={cart}
-                              setCart={setCart}
-                              productNum={productNum}
-                              setProductNum={setProductNum}
-                              priceTotal={priceTotal}
-                              setPriceTotal={setPriceTotal}
-                          />
-                      } 
-                    />
-                    <Route path="/ordered" element={<Ordered />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header productNum={productNum} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route 
+            path="/shop" 
+            element={
+              <Shop>
+                {productList.map((product, e) => {
+                  return (
+                    <div className="product" key={uniqid()}>
+                      <img className="product-img" alt={product.name} src={product.image} />
+                      <div className="product-info">
+                        <h3 className="product-title">{product.name}</h3>
+                        <p className="product-price">{`$${product.price} ${product.rate}`}</p>
+                      </div>
+                      <p className="product-description">{product.description}</p>
+                      <button 
+                        className="add-cart-btn"
+                        e={e}
+                        onClick={(e) => handleAddToCart(e)}
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
+                  )
+                })}
+              </Shop>
+            } 
+          />
+          <Route 
+            path="/cart" 
+            element={
+              <Cart 
+                cart={cart}
+                setCart={setCart}
+                productNum={productNum}
+                setProductNum={setProductNum}
+                priceTotal={priceTotal}
+              />
+            } 
+          />
+          <Route path="/ordered" element={<Ordered />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,28 +1,9 @@
 import React from "react";
-import uniqid from 'uniqid';
 
-const Shop = ({ handleAddToCart, productList }) => {
+const Shop = (props) => {
     return (
         <div className="shop">
-            {productList.map((product, e) => {
-                return (
-                    <div className="product" key={uniqid()}>
-                        <img className="product-img" alt={product.name} src={product.image} />
-                        <div className="product-info">
-                            <h3 className="product-title">{product.name}</h3>
-                            <p className="product-price">{`$${product.price} ${product.rate}`}</p>
-                        </div>
-                        <p className="product-description">{product.description}</p>
-                        <button 
-                            className="add-cart-btn"
-                            e={e}
-                            onClick={(e) => handleAddToCart(e)}
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                )
-            })}
+            {props.children}
         </div>
     )
 }
